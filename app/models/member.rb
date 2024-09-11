06c2ac6a405953de9a00ae75ac4244780dac387a
Corentin Dubois <corentindubois22@gmail.com>
2024-09-11 15:05:05 -0600

    pfixed weird group show error

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,6 +3,7 @@ class Member < ApplicationRecord
   belongs_to :group
   has_many :progresses, dependent: :destroy
 
-  validates :user_id, uniqueness: true
-  validates :group_id, uniqueness: true
+  # validates :user_id, uniqueness: true
+  # validates :group_id, uniqueness: true
+  validates :user_id, uniqueness: { scope: :group_id, message: "ya está en este grupo" }
 end
