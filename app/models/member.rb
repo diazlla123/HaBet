8189c1377b830945c2081b291f3a9300e316af4b
cesar jordi <jordybc15@gmail.com>
2024-09-18 12:16:10 -0500

    ranking real time improved

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,12 +3,12 @@ class Member < ApplicationRecord
   belongs_to :group
   has_many :progresses, dependent: :destroy
 
-  ### added to make the pushinments and rewards tracking feacture 
+  ### added to make the pushinments and rewards tracking feacture
   has_many :member_punishments, dependent: :destroy
   has_many :punishments, through: :member_punishments
   has_many :member_rewards, dependent: :destroy
   has_many :rewards, through: :member_rewards
-  
+
   has_one_attached :photo
 
   # validates :user_id, uniqueness: true
@@ -18,6 +18,12 @@ class Member < ApplicationRecord
   COLORS = ["#AEC6CF", "#77DD77", "#F7C6C7", "#B39EB5", "#FDFD96", "#FFABAB", "#FFDAB9", "#B2DFDB", "#B2DFDB", "#B2DFDB"]
 
   before_create :assign_random_color
+
+  def sorted_positions
+    tasks = Task.where(group_id: group.id)
+    ranking_calculation = RankingCalculation.new(tasks, group.id)
+    ranking_calculation.position_table
+  end
 
   private
 
--- app/models/progress.rb
+++ app/models/progress.rb
@@ -1,4 +1,36 @@
 class Progress < ApplicationRecord
   belongs_to :task
   belongs_to :member
+
+  after_create_commit :broadcast_ranking
+  after_update_commit :broadcast_ranking
+
+  after_create_commit :broadcast_incentives
+  after_update_commit :broadcast_incentives
+
+  private
+
+  def broadcast_ranking
+    broadcast_update_to "group_#{member.group.id}_rank",
+                        target: "group_ranking",
+                        partial: "members/member_rank",
+                        locals: { sorted_positions: member.sorted_positions, member_user: member.user, group_id: member.group.id }
+  end
+
+  def broadcast_incentives
+    broadcast_update_to "group_#{member.group.id}_incentives",
+                        target: "member_incentives",
+                        partial: "members/incentives",
+                        locals: { member_punishments: member_punishments, member_rewards: member_rewards, group: member.group }
+  end
+
+  def member_punishments
+    member.user.members.find_by(group_id: member.group.id).member_punishments.includes(:punishment)
+    # member.member_punishments.includes(:punishment)
+  end
+
+  def member_rewards
+    member.user.members.find_by(group_id: member.group.id).member_rewards.includes(:reward)
+    # member.member_punishments.includes(:punishment)
+  end
 end
